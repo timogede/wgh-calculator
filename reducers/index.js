47b8bc17be71e8c03e2e19cb0e85b982d127d465
timogede <timo@timogede.com>
2021-02-01 16:09:17 +0100

    backend stuff on mac

---
+++ reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import subscribers from "./subscribers";
+
+export default combineReducers({
+  subscribers,
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import './index.scss';
-import App from './App';
+import reducers from "./reducers";
 
+import "./index.scss";
+import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
