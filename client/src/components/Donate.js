abd1bb7ac3a572ce3179c8a4400b77bbd546e974
timogede <timo@timogede.com>
2021-08-24 22:31:31 +0200

    donate work

--- client/src/components/Donate.js
+++ client/src/components/Donate.js
@@ -12,22 +12,24 @@ const donate = () => {
 
   return (
     <React.Fragment>
-      <div className="donate container">
+      <div className="donate container container--margin">
         <div className="donate_inside container__inside">
           <div className="donate__columns">
             <div className="donate__column donate__column-1">
-              <h2>handicap.report unterstützen</h2>
-              <p>
-                <strong>DID YOU KNOW:</strong> If only 1% of MyGolfSpy readers
-                donated $25, we would be able to become completely independent
-                in 12-months. With every donation, you create change. Would you
-                be willing to help by giving a donation? Every dollar will help.
-                Make a donation to support our independent and expert golf
-                equipment research.
-                <strong>
-                  A PayPal account is not required in order to donate.
-                </strong>
-              </p>
+              <div className="donate__text-box">
+                <h2>handicap.report unterstützen</h2>
+                <p>
+                  <strong>DID YOU KNOW:</strong> If only 1% of MyGolfSpy readers
+                  donated $25, we would be able to become completely independent
+                  in 12-months. With every donation, you create change. Would
+                  you be willing to help by giving a donation? Every dollar will
+                  help. Make a donation to support our independent and expert
+                  golf equipment research.
+                  <strong>
+                    A PayPal account is not required in order to donate.
+                  </strong>
+                </p>
+              </div>
             </div>
             <div className="donate__column donate__column-2">
               <div className="donate__donate-box">
@@ -36,17 +38,58 @@ const donate = () => {
                   <div className="donate__option-column donate__option-column-1">
                     <p>Menge</p>
                     <label class="donate__radio">
+                      <input type="radio" name="amount_option" value="5.00" />
+                      <span class="checkmark"></span>
+                      €5
+                    </label>
+                    <label class="donate__radio">
+                      <input type="radio" name="amount_option" value="10" />
+                      <span class="checkmark"></span>
+                      €10
+                    </label>
+                    <label class="donate__radio">
+                      <input type="radio" name="amount_option" value="20" />
+                      <span class="checkmark"></span>
+                      €20
+                    </label>
+                    <label class="donate__radio">
+                      <input type="radio" name="amount_option" value="50" />
+                      <span class="checkmark"></span>
+                      €50
+                    </label>
+                    <label class="donate__radio">
+                      <input type="radio" name="amount_option" value="other" />
+                      <span class="checkmark"></span>
+                      anderer Betrag
+                    </label>
+                    <div className="donate__other">
+                      <input
+                        type="text"
+                        placeholder="€35"
+                        name="amount_option--other-value"
+                      />
+                    </div>
+                  </div>
+                  <div className="donate__option-column donate__option-column-2">
+                    <label class="donate__radio">
                       <input
                         type="radio"
-                        name="amount_option"
-                        checked="checked"
-                        value="25.00"
+                        name="frequenzy_option"
+                        value="other"
                       />
                       <span class="checkmark"></span>
-                      $25.00
+                      einmalige Spende
+                    </label>
+                    <label class="donate__radio">
+                      <input
+                        type="radio"
+                        name="frequenzy_option"
+                        value="other"
+                      />
+                      <span class="checkmark"></span>
+                      monatliche Spende
                     </label>
                   </div>
-                  <div className="donate__option-column donate__option-column-2"></div>
                 </div>
               </div>
             </div>
